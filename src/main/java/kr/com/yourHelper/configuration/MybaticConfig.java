6ff525a9021715480b1708a0a8baebec50963326
superpil <peel0220@gmail.com>
2020-05-21 23:00:07 +0900

    security설정미완성

--- src/main/java/kr/com/yourHelper/configuration/MybaticConfig.java
+++ src/main/java/kr/com/yourHelper/configuration/MybaticConfig.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:/application.properties")
-public class MybaticConfiguration {
+public class MybaticConfig {
 	
 	@Autowired
 	public ApplicationContext applicationContext;
---
+++ src/main/java/kr/com/yourHelper/configuration/SecurityConfig.java
@@ -0,0 +1,6 @@
+package kr.com.yourHelper.configuration;
+
+@EnableWebSrcurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+}
